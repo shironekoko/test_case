583e46cee8417f9bb52fddfa7c2a9a9e22ca5f89
shironekoko <ohmohm102549@gmail.com>
2025-03-02 21:35:23 +0700

    testalterchar

---
+++ test_alterchar.py
@@ -0,0 +1,30 @@
+import unittest
+from function import alternatingCharacters
+
+class TestAlternatingCharacters(unittest.TestCase):
+
+    def test_alternating(self):
+        self.assertEqual(alternatingCharacters("ababab"), 0)
+        self.assertEqual(alternatingCharacters("abab"), 0)
+        self.assertEqual(alternatingCharacters("a"), 0)
+        self.assertEqual(alternatingCharacters("b"), 0)
+
+    def test_non_alternating(self):
+        self.assertEqual(alternatingCharacters("aaabaaaa"), 5)
+        self.assertEqual(alternatingCharacters("bbbb"), 3)
+        self.assertEqual(alternatingCharacters("aaaaa"), 4)
+        self.assertEqual(alternatingCharacters("aabb"), 2)
+
+    def test_edge_cases(self):
+        self.assertEqual(alternatingCharacters(""), 0)
+        self.assertEqual(alternatingCharacters("bbbbbb"), 5)
+        self.assertEqual(alternatingCharacters("abababab"), 0)
+
+    def test_mixed_cases(self):
+        self.assertEqual(alternatingCharacters("aab"), 1)
+        self.assertEqual(alternatingCharacters("ababa"), 0)
+        self.assertEqual(alternatingCharacters("abbab"), 1)
+        self.assertEqual(alternatingCharacters("abcba"), 2)
+
+if __name__ == '__main__':
+    unittest.main()
--- test_funnyString.py
+++ test_funnyString.py
@@ -12,7 +12,20 @@ class TestFunnyString(unittest.TestCase):
     def test_edge_cases(self):
         self.assertEqual(funnyString("zxcvbnmlkjhgfdsa"), "Not Funny")
         self.assertEqual(funnyString("zyxw"), "Funny")
-        self.assertEqual(funnyString("axby"), "Funny") 
+        self.assertEqual(funnyString("axby"), "Not Funny")
+
+    def test_additional_cases(self):
+        self.assertEqual(funnyString("ab"), "Funny")
+        self.assertEqual(funnyString("ba"), "Funny")
+        self.assertEqual(funnyString("abc"), "Funny")
+        self.assertEqual(funnyString("cba"), "Funny")
+        self.assertEqual(funnyString("abcdcba"), "Funny")
+        self.assertEqual(funnyString("abccba"), "Funny")
+
+    def test_special_cases(self):
+        self.assertEqual(funnyString(""), "Funny")  
+        self.assertEqual(funnyString("a"*1000), "Funny")  
+        self.assertEqual(funnyString("ab"*500), "Funny")
 
 if __name__ == '__main__':
     unittest.main()
